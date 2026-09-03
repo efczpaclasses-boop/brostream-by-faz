284e5a78cbef31d747c7b8c3114adf8eb0644060
Faz <efczpaclasses-boop@users.noreply.github.com>
2026-09-04 00:44:43 +0400

    Pin working CloudStream Gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,9 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:8.7.3")
-        classpath("com.github.recloudstream:gradle:-SNAPSHOT")
+        // Pin a known-good build: JitPack's moving snapshots periodically point
+        // at metadata before the matching Gradle plugin artifact is available.
+        classpath("com.github.recloudstream:gradle:81b1d424d2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.3.0")
     }
 }
